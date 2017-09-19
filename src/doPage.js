1c440ffcb36f757b5b82884a57d50c5c374ebcb5
spencermountain <spencermountain@gmail.com>
2017-09-19 14:39:28 -0400

    encoding for #15

--- src/doPage.js
+++ src/doPage.js
@@ -2,20 +2,56 @@ let wtf = require('wtf_wikipedia')
 //these methods may run in their own processes
 
 //https://stackoverflow.com/questions/12397118/mongodb-dot-in-key-name/30254815#30254815
-const encodeKey = function(key) {
-  return key
-    .replace('\\', '\\\\')
-    .replace(/^$/, '\\u0024')
-    .replace(/\./, '\\u002e')
+const encodeStr = function(str) {
+  return str
+    .replace(/\\/g, '\\\\')
+    .replace(/^\$/, '\\u0024')
+    .replace(/\./g, '\\u002e')
+}
+
+//tables & infoboxes both could potentially have unsafe keys
+const encodeData = function(data) {
+  data = data || {}
+  //encode keys in infoboxes
+  if (data.infoboxes && data.infoboxes.length > 0) {
+    data.infoboxes.forEach(info => {
+      let keys = Object.keys(info.data)
+      keys.forEach(k => {
+        if (k !== encodeStr(k)) {
+          info.data[encodeStr(k)] = info.data[k]
+          delete info.data[k]
+        }
+      })
+    })
+  }
+  //cleanup tables
+  if (data.sections && data.sections.length > 0) {
+    data.sections.forEach(o => {
+      if (o.tables && o.tables.length > 0) {
+        o.tables.forEach(table => {
+          table.forEach(row => {
+            let keys = Object.keys(row)
+            keys.forEach(k => {
+              if (k !== encodeStr(k)) {
+                row[encodeStr(k)] = row[k]
+                delete row[k]
+              }
+            })
+          })
+        })
+      }
+    })
+  }
+  return data
 }
 
 //get parsed json from the wiki markup
 const parse = function(options, cb) {
   let data = wtf.parse(options.script)
-  let title = encodeKey(options.title || '')
-  console.log(title)
-  data.title = title
-  data._id = title
+  data = encodeData(data)
+  // console.log(data)
+  data.title = options.title
+  data._id = encodeStr(options.title)
   options.collection.insert(data, function(e) {
     if (e) {
       console.log(e)
