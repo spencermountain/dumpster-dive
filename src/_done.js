27c73f4b5fff2cf99316a57936c73a18772520dc
devrim <devrim@koding.com>
2018-02-09 10:24:27 -0800

    fixes on re-write
    - redis worker no longer needed
    - async operations
    - collect errors per batch (insertMany results/errors are not always errors and/or results in our case, eg duplicates not being inserted is ok - let's collect them in errors, then deal with them after done() fn is called
    - auto_skip amount of docs that are in the main collection
    
    tbd: split the file in chunks and run dump in multiple threads.

--- src/_done.js
+++ src/_done.js
@@ -1,11 +1,8 @@
 
 //function to pretty-print when finished..
-const done = function(options, callback) {
+const done = async function(collection, callback) {
   console.log('\n=================done!=================\n');
-  options.collection.count().then(count => {
-    console.log(count + "  pages stored in db '" + options.db + "'");
-    options.database.close();
-    callback()
-  });
+  console.log(await collection.count() + "  pages stored in db '" + options.db + "'");
+  return
 };
 module.exports = done
