aeeba879ea2afab14ae671a6561b31d055780a89
spencer kelly <spencermountain@gmail.com>
2018-10-02 18:30:54 -0400

    20s interval worker logging 4.0.1

--- src/worker/index.js
+++ src/worker/index.js
@@ -5,7 +5,7 @@ const parsePage = require('./01-parsePage');
 const parseWiki = require('./02-parseWiki');
 const writeDb = require('./03-write-db');
 const jsonfn = require('jsonfn').JSONfn;
-const niceNum = require('../lib/fns').niceNumber
+const niceNum = require('../lib/fns').niceNumber;
 
 const doSection = async (optionStr, workerCount, workerNum) => {
   let options = jsonfn.parse(optionStr);
@@ -18,14 +18,14 @@ const doSection = async (optionStr, workerCount, workerNum) => {
     redirects: 0,
     ns: 0,
     disambig: 0,
-  }
+  };
   this.logger = setInterval(() => {
-    console.log(`      ${chalk.yellow('─── worker #' + workerNum + ' ───')}: `)
-    console.log(`         ${chalk.green('+' + niceNum(this.counts.pages))} ${chalk.yellow('pages')}`)
-    console.log(`         ${chalk.magenta(niceNum(this.counts.redirects * -1))} ${chalk.yellow('redirects')}`)
-    console.log(`         ${chalk.magenta(niceNum(this.counts.disambig * -1))} ${chalk.yellow('disambig')}`)
-    console.log(`         ${chalk.magenta(niceNum(this.counts.ns * -1))} ${chalk.yellow('ns')}`)
-  }, 20000 + (workerNum * 15))
+    console.log(`      ${chalk.yellow('─── worker #' + workerNum + ' ───')}: `);
+    console.log(`         ${chalk.green('+' + niceNum(this.counts.pages))} ${chalk.grey('pages')}`);
+    console.log(`         ${chalk.magenta(niceNum(this.counts.redirects * -1))} ${chalk.grey('redirects')}`);
+    console.log(`         ${chalk.magenta(niceNum(this.counts.disambig * -1))} ${chalk.grey('disambig')}`);
+    console.log(`         ${chalk.magenta(niceNum(this.counts.ns * -1))} ${chalk.grey('ns')}`);
+  }, 20000 + (workerNum * 15));
   // console.log(`#${workerNum} -   ${start}% → ${end}%`)
   let driver = {
     file: options.file,
@@ -47,7 +47,7 @@ const doSection = async (optionStr, workerCount, workerNum) => {
       }
       if (pages.length >= options.batch_size) {
         writeDb(options, pages, workerNum).then(() => {
-          this.counts.pages += pages.length
+          this.counts.pages += pages.length;
           pages = [];
           resume();
         });
@@ -71,7 +71,7 @@ const doSection = async (optionStr, workerCount, workerNum) => {
     console.log('\n\n');
   });
   p.then(async () => { //on done
-    clearInterval(this.logger)
+    clearInterval(this.logger);
     // insert the remaining pages
     if (pages.length > 0) {
       await writeDb(options, pages, workerNum);
