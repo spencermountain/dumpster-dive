7ad54c7931eeee44efadfc74f5247e9af817f41a
Vid Kersic <vid.kersic@yahoo.com>
2021-02-12 21:45:09 +0100

    fix db_url option issue for #92

--- src/lib/open-db.js
+++ src/lib/open-db.js
@@ -6,7 +6,7 @@ const openDb = async function (options) {
   if (!options.db) {
     console.warn('\n--missing db name--');
   }
-  const url = config.db_url + options.db;
+  const url = (options.db_url) ? options.db_url : config.db_url;
 
   return new Promise((resolve, reject) => {
     MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true }, function (
